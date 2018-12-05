fcb259f2a4f72cde7623918f1d7552d982db4f1f
alberto <albpanig@tiscali.it>
2018-12-05 23:05:41 +0100

    1

---
+++ 1/src/LeggiEStampaEta.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class LeggiEStampaEta
+{
+    public static void main (String args[])
+    {
+        // Impostazione per lo standard input
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader Tastiera = new BufferedReader(input);
+        int Eta;
+        String TestoLetto;
+        
+        try {
+               System.out.print("Digita la tua eta': ");
+               TestoLetto=Tastiera.readLine();
+               Eta=Integer.valueOf(TestoLetto).intValue();
+        }
+        catch (Exception e)
+        {
+            System.out.println("\nNon hai digitato un numero!");
+            return;
+        }
+        System.out.println("La tua eta' e': " + Eta);
+    }
+}
